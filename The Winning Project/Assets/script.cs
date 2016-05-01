342b714b9dc36cdf1271fc141d0d3d923ee9c0b9
Regina Ongowarsito <regina.ongowarsito@gmail.com>
2016-05-01 03:47:35 -0700

    did some things with test cube

---
+++ The Winning Project/Assets/script.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class script : MonoBehaviour {
+
+	public float speed;
+    private Rigidbody rb;
+	public Text countText;
+
+	// Use this for initialization
+	void Start () {
+		//rb = GetComponent<Rigidbody>();
+		//countText.text = "yas";
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float moveHor = 0;
+		//Vector3 move = new Vector3(Vector3.forward * speed, 0.0f, 0.0f);
+
+		//if (Input.GetMouseButtonDown (0)) {
+		//if (Input.GetMouseButtonDown (0)) {
+		//	moveHor = 40;
+		//} else if (Input.GetMouseButtonUp (0)) {
+		//	moveHor = -1;
+		//}
+
+		transform.Translate (Vector3.back * speed);
+			
+
+		//rb.AddForce (move);
+
+		//Transform 
+
+	}
+		
+
+}
